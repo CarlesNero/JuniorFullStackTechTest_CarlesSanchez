3dbcb5f554116713e0ee8f10d335367de7c11231
CarlesNero <carlessanchez.vae@gmail.com>
2025-09-16 17:44:31 +0000

    cambio origen

--- VolskwagenTTBackend/src/main/kotlin/com/volskwagenTTBackend/VolskwagenTTBackend/config/SecurityConfig.kt
+++ VolskwagenTTBackend/src/main/kotlin/com/volskwagenTTBackend/VolskwagenTTBackend/config/SecurityConfig.kt
@@ -36,7 +36,7 @@ class SecurityConfig {
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
         config.allowCredentials = true
-        config.addAllowedOrigin("http://localhost:5173")
+        config.addAllowedOrigin("http://tictactoe-front.captain.csanchezm.es")
         config.addAllowedHeader("*")
         config.addAllowedMethod("*")
         source.registerCorsConfiguration("/**", config)
