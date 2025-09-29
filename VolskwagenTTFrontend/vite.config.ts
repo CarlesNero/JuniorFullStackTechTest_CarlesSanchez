f7b7ad1387a84d11d667ca0dbc6d1c30172cd1e9
CarlesNero <carlessanchez.vae@gmail.com>
2025-09-29 11:32:26 +0200

    base

--- VolskwagenTTFrontend/vite.config.ts
+++ VolskwagenTTFrontend/vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vitest/config"; 
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
   test: {
     environment: "jsdom",
     globals: true,
